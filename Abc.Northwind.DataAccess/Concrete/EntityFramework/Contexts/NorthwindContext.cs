52b7b435b949a2d8c36a5e683f6244703a4596ad
muratsesen <muratsesen@4alabs.io>
2019-03-20 14:43:25 +0300

    implementing the context class

---
+++ Abc.Northwind.DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using Abc.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abc.Northwind.DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Abc.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ Abc.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,6 @@
-﻿using Abc.Northwind.DataAccess.Abstract;
+﻿using Abc.Core.DataAccess.EntityFramework;
+using Abc.Northwind.DataAccess.Abstract;
+using Abc.Northwind.DataAccess.Concrete.EntityFramework.Contexts;
 using Abc.Northwind.Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -9,31 +11,8 @@ using System.Threading.Tasks;
 
 namespace Abc.Northwind.DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
     {
-        public Product Add(Product entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(Product entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Product> GetList(Expression<Func<Product, bool>> filter = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Product Update(Product entity)
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
